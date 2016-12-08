6cb29dfaf16ef4fa552effd6666a0475f1068ae3
xiaoyin <xiaoyin1992@163.com>
2016-12-08 20:51:55 +0800

    Signed-off-by: xiaoyin <xiaoyin1992@163.com>

--- js/myDeng.js
+++
@@ -1,8 +0,0 @@
-window.onload=function(){
-var omD_arrow=document.getElementById("mD_arrow");	
-omD_arrow.addEventListener("click",omD_back,false);
-function omD_back(){
-	window.history.back();
-}
-	
-}
